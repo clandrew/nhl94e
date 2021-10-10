2fee5e883cec701fa8b4b578ceadf3e630995777
ClAndrew <cmlandrews@gmail.com>
2021-10-09 22:45:23 -0700

    Remove unnecessary code

--- CopyProfileImages.asm
+++ CopyProfileImages.asm
@@ -9,25 +9,6 @@
 // This function is called twice when loading the GAME SETUP screen, once per team.
 //
 
-$9D/CC42 A5 A5       LDA $A5    [$00:00A5]  
-$9D/CC44 48          PHA                    // Politely back up $A5 because LookupPlayerIndexForPlayerWithProfile is going to scramble it.
-
-$9D/CC45 A2 0A 00    LDX #$000A				// OffsetCounter=10. This gets used to index in an array of 2-byte elements.
-
-//ForEachProfileImagePlayerOnTeam:			// Probably don't need this loop!
-$9D/CC48 DA          PHX                     
-
-$9D/CC4F A6 91       LDX $91    [$00:0091]   // Load the choice of HomeOrAway. 0 == home, 2 == away					     
-$9D/CC51 BD 98 1C    LDA $1C98,x[$9F:1C98]   // Load the index of CurrentTeam. 
-
-$9D/CC54 9F 6C 34 7E STA $7E346C,x[$7E:346C] // Stash the CurrentTeamIndex in the appropriate slot for home versus away. Maybe not necessary when doing the load
-
-$9D/CCA5 FA          PLX                     // Pull OffsetCounter
-$9D/CCA6 CA          DEX                     // OffsetCounter -= 2
-$9D/CCA7 CA          DEX                     
-
-$9D/CCA8 10 F1       BPL $F1            // if OffsetCounter > 0, goto ForEachProfileImagePlayerOnTeam
-
 //Done:
 // Do an absolutely stupid copy of 0x600 bytes from A3C500 to 7F5100 for home, to 7F2D00 for away.
 // Source: $0C-$0E
@@ -61,6 +42,4 @@ B7 0C				LDA [$0C],y
 88					DEY
 10 F9				BPL CopyLoop
 
-$9D/CCAA 68          PLA                     
-$9D/CCAB 85 A5       STA $A5    [$00:00A5]   
 $9D/CCAD 6B          RTL					// Restore $A5 and return
