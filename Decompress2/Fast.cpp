faa1f31449c7a1fd830c4e6a9c4ed38f3b9549eb
clandrew <cmlandrews@gmail.com>
2022-11-05 23:11:13 -0400

    Temp change

--- Decompress2/Fast.cpp
+++ Decompress2/Fast.cpp
@@ -463,6 +463,22 @@ namespace Fast
             /* Case 8 */ { {2, 3, 4, 5, 6, 7, 8, 0, 0}, {2, 2, 2, 2, 2, 2, 2, 0, 0},        {2, 4, 8, 16, 32, 64, 128, 0, 0},  },
         };
 
+    struct CaseTable8Entry
+    {
+        int Cond;
+        int Lower;
+        int IterCount;
+    } caseTable8Entries[] = {
+        {2, 0, 2},      // x==2   
+        {4, 7, 4},      // x==4   
+        {6, 6, 4},      // x==6    
+        {8, 5, 5},      // x==8    
+        {10, 4, 7},     // x==10    
+        {12, 3, 7},     // x==12   
+        {14, 2, 8},     // x==14   
+        {16, 1, 9},     // x==16    
+    };
+
     void Fn_80BBB3()
     {
         // This is a sizeable function, a.k.a. 'the monstrosity'.
@@ -616,163 +632,32 @@ namespace Fast
             loaded16 = LoadMem6b();
             a = loaded16.Data16;
 
-            // Switchcase 8 /////////////////////////////////////////////
-            if (x == 2)
+            for (int iCase = 0; iCase < _countof(caseTable8Entries); ++iCase)
             {
-                for (int i = 0; i < 2; ++i)
+                if (x == caseTable8Entries[iCase].Cond)
                 {
-                    a *= 2;
-                    if (i == 0)
+                    int lower = caseTable8Entries[iCase].Lower;
+                    int iterCount = caseTable8Entries[iCase].IterCount;
+                    for (int i = lower; i < lower + iterCount; ++i)
                     {
-                        LoadNextFrom0CInc();
-                    }
+                        a *= 2;
+                        if (i == 0)
+                        {
+                            LoadNextFrom0CInc();
+                        }
 
-                    y--;
-                    if (y == 0)
-                    {
-                        LoadNextFrom0600();
-                        nextCaseIndex = (i % 8) + 1;
-                        goto label_mainSwitchCaseTable;
+                        y--;
+                        if (y == 0)
+                        {
+                            LoadNextFrom0600();
+                            nextCaseIndex = (i % 8) + 1;
+                            goto label_mainSwitchCaseTable;
+                        }
                     }
+                    break;
                 }
             }
-            else if (x == 4)
-            {
-                for (int i = 7; i < 7 + 4; ++i)
-                {
-                    a *= 2;
-                    if (i == 8)
-                    {
-                        LoadNextFrom0CInc();
-                    }
 
-                    y--;
-                    if (y == 0)
-                    {
-                        LoadNextFrom0600();
-                        nextCaseIndex = (i % 8) + 1;
-                        goto label_mainSwitchCaseTable;
-                    }
-                }
-            }
-            else if (x == 6)
-            {
-                for (int i = 6; i < 6 + 4; ++i)
-                {
-                    a *= 2;
-                    if (i == 8)
-                    {
-                        LoadNextFrom0CInc();
-                    }
-
-                    y--;
-                    if (y == 0)
-                    {
-                        LoadNextFrom0600();
-                        nextCaseIndex = (i % 8) + 1;
-                        goto label_mainSwitchCaseTable;
-                    }
-                }
-            }
-            else if (x == 8)
-            {
-                for (int i = 5; i < 5 + 5; ++i)
-                {
-                    a *= 2;
-                    if (i == 8)
-                    {
-                        LoadNextFrom0CInc();
-                    }
-
-                    y--;
-                    if (y == 0)
-                    {
-                        LoadNextFrom0600();
-                        nextCaseIndex = (i % 8) + 1;
-                        goto label_mainSwitchCaseTable;
-                    }
-                }
-            }
-            else if (x == 0xA)
-            {
-                for (int i = 4; i < 4 + 7; ++i)
-                {
-                    a *= 2;
-                    if (i == 8)
-                    {
-                        LoadNextFrom0CInc();
-                    }
-
-                    y--;
-                    if (y == 0)
-                    {
-                        LoadNextFrom0600();
-                        nextCaseIndex = (i % 8) + 1;
-                        goto label_mainSwitchCaseTable;
-                    }
-                }
-            }
-            else if (x == 0xC)
-            {
-                for (int i = 3; i < 3 + 7; ++i)
-                {
-                    a *= 2;
-                    if (i == 8)
-                    {
-                        LoadNextFrom0CInc();
-                    }
-
-                    y--;
-                    if (y == 0)
-                    {
-                        LoadNextFrom0600();
-                        nextCaseIndex = (i % 8) + 1;
-                        goto label_mainSwitchCaseTable;
-                    }
-                }
-            }
-            else if (x == 0xE)
-            {
-                for (int i = 2; i < 2 + 8; ++i)
-                {
-                    a *= 2;
-                    if (i == 8)
-                    {
-                        LoadNextFrom0CInc();
-                    }
-
-                    y--;
-                    if (y == 0)
-                    {
-                        LoadNextFrom0600();
-                        nextCaseIndex = (i % 8) + 1;
-                        goto label_mainSwitchCaseTable;
-                    }
-                }
-            }
-            else if (x == 0x10)
-            {
-                for (int i = 2; i < 2 + 9; ++i)
-                {
-                    a *= 2;
-                    if (i == 8)
-                    {
-                        LoadNextFrom0CInc();
-                    }
-
-                    y--;
-                    if (y == 0)
-                    {
-                        LoadNextFrom0600();
-                        nextCaseIndex = i;
-                        goto label_mainSwitchCaseTable;
-                    }
-                }
-            }
-            else
-            {
-                __debugbreak(); // notimpl
-            }
 
         label_C17C_WriteOutput_CheckIfDone:
             y = mem73 >> 8;
