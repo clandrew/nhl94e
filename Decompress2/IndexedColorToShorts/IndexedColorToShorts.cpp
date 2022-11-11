56c5f4c4486f0d82b4626457a8201773c8adba64
ClAndrew <cmlandrews@gmail.com>
2022-11-10 18:57:32 -0800

    Save

--- Decompress2/IndexedColorToShorts/IndexedColorToShorts.cpp
+++ Decompress2/IndexedColorToShorts/IndexedColorToShorts.cpp
@@ -9,13 +9,30 @@ struct TestCase
     unsigned short Shorts[2];
 } testCases[] =
 {
-    {0, 0, 0, 0, 0, 0}
+    //{0x00, 0x00, 0x00, 0x00, 0x0000, 0x0000},
+    //{0x10, 0x10, 0x10, 0x00, 0x0007, 0x0000},
+    {0x00, 0x00, 0x00, 0x20, 0x0080, 0x0000},
 };
 
 void IndexedColorToShorts(unsigned char i0, unsigned char i1, unsigned char i2, unsigned char i3, unsigned short* pS0, unsigned short* pS1)
 {
     *pS0 = 0;
     *pS1 = 0;
+
+    // Don't overflow the first
+    // 0x4FFF would work
+
+    // x2   9FFE  n
+    // x2   3FFC  o
+    // x2   7FF8  n
+    // x2   FFF0  n
+
+    // Overflow the 2nd
+
+    // Only set one bit of the short
+    int ResultComponent = 0x20;
+
+    *pS0 |= 0x80;
 }
 
 int main()
